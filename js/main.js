4aa9c33a327aecd03e9bf5cdc32c9fe5bcef88bc
GirdaViktor <VSGirda@gmail.com>
2022-02-06 22:46:58 +0300

    В файле main.js на основе написанных по заданию ранее вспомогательных функций напишите необходимые функции для создания массива из 25 сгенерированных объектов. Каждый объект массива — описание фотографии, опубликованной пользователем.
    Структура каждого объекта должна быть следующей:
    
    id, число — идентификатор описания. Это число от 1 до 25. Идентификаторы не должны повторяться.
    
    url, строка — адрес картинки вида photos/{{i}}.jpg, где {{i}} — это число от 1 до 25. Адреса картинок не должны повторяться.
    
    description, строка — описание фотографии. Описание придумайте самостоятельно.
    
    likes, число — количество лайков, поставленных фотографии. Случайное число от 15 до 200.
    
    comments, массив объектов — список комментариев, оставленных другими пользователями к этой фотографии. Количество комментариев к каждой фотографии вы определяете на своё усмотрение. Все комментарии генерируются случайным образом. Пример описания объекта с комментарием:
    
    {
      id: 135,
      avatar: 'img/avatar-6.svg',
      message: 'В целом всё неплохо. Но не всё.',
      name: 'Артём',
    }
    У каждого комментария есть идентификатор — id — случайное число. Идентификаторы не должны повторяться.
    
    Поле avatar — это строка, значение которой формируется по правилу img/avatar-{{случайное число от 1 до 6}}.svg. Аватарки подготовлены в директории img.
    
    Для формирования текста комментария — message — вам необходимо взять одно или два случайных предложения из представленных ниже:
    
    Всё отлично!
    В целом всё неплохо. Но не всё.
    Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.
    Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.
    Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.
    Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!
    Имена авторов также должны быть случайными. Набор имён для комментаторов составьте сами. Подставляйте случайное имя в поле name.

--- js/main.js
+++ js/main.js
@@ -15,4 +15,109 @@ const getRandomInteger = (min, max) => {
 
 const getCheckLengthString = (str) => str.length < 140;
 
-// Пробую на маке запушить
+/* Module4-task1 */
+const initialData = {
+  countPreview: {
+    start: 1,
+    end: 25,
+  },
+  names: [
+    `Анна`,
+    `Мария`,
+    `Александр`,
+    `Влад`,
+    `Константин`,
+    `Егор`,
+    `Милена`,
+    `Эсмеральда`,
+  ],
+  comments: {
+    count: {
+      min: 0,
+      max: 135,
+    },
+    avatar: {
+      avatarMin: 1,
+      avatarMax: 6,
+    },
+    message: {
+      length: {
+        min: 1,
+        max: 2,
+      },
+      message: [
+        `Всё отлично!`,
+        `В целом всё неплохо. Но не всё.`,
+        `Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.`,
+        `Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.`,
+        `Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.`,
+        `Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!`,
+      ],
+    },
+  },
+  likes: {
+    min: 15,
+    max: 200,
+  },
+};
+
+const getShuffle = (arr) => {
+  const newArr = arr.slice();
+
+  for (let i = newArr.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [newArr[i], newArr[j]] = [newArr[j], newArr[i]];
+  }
+
+  return newArr;
+};
+
+const getMessage = (obj) => {
+  const {min, max} = obj.comments.message.length;
+  const countMessage = getRandomInteger(min, max);
+  const messages = [];
+
+  for (let i = min; i <= countMessage; i++) {
+    messages.push(getShuffle(obj.comments.message.message)[i]);
+  }
+
+  return messages;
+};
+
+const getComments = (obj) => {
+  const comments = [];
+  const {min, max} = obj.comments.count;
+  const {avatarMin, avatarMax} = obj.comments.avatar;
+  const countComment = getRandomInteger(min, max);
+
+  for (let i = min; i <= countComment; i++) {
+    comments.push({
+      id: i,
+      avatar: `img/avatar-${getRandomInteger(avatarMin, avatarMax)}.svg`,
+      message: getMessage(obj),
+      name: obj.names[getRandomInteger(obj.names.length - 1)],
+    });
+  }
+
+  return comments;
+};
+
+const createData = (obj) => {
+  const {start, end} = obj.countPreview;
+  const data = [];
+  const {min, max} = obj.likes;
+
+  for (let i = start; i <= end; i++) {
+    data.push({
+      id: i,
+      url: `photos/${i}.jpg`,
+      description: `Какое-то описание для фотографии`,
+      likes: getRandomInteger(min, max),
+      comments: getComments(obj),
+    });
+  }
+
+  return data;
+};
+
+console.log(createData(initialData));
